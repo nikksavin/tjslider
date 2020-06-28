e9b321e3e5a9e07f9f607664cc93ac9ce9cc06c8
agentnice <mykolakratasyk@gmail.com>
2020-06-28 12:03:48 +0300

    update

--- js/script.js
+++ js/script.js
@@ -4,13 +4,13 @@ $('.slider__left').slick({
     slidesToScroll: 1,
     asNavFor: '.slider__right',
     variableWidth: true,
-  });
+});
 
-  $('.slider__right').slick({
+$('.slider__right').slick({
     infinite: true,
     slidesToShow: 1,
     slidesToScroll: 1,
     dots: true,
     asNavFor: '.slider__left',
     arrows: false
-  });
+});
